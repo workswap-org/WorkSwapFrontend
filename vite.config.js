a1c7e45df373e430fb840dfcb1fc2a9641498181
Kirya <kkodolov40@gmail.com>
2025-09-09 01:14:57 +0300

    фикс локал разрешённых доменов

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,11 @@ export default defineConfig({
         },
     },
     server: {
-        port: 8080
+        host: '0.0.0.0',
+        port: 8080,
+        preview: {
+            allowedHosts: ['workswap.org'] // твой домен
+        }
     },
     define: {
         global: 'window'
