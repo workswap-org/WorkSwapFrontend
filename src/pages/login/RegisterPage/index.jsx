02d5fc5b22bcba621f8bb8458de05e23cc13020f
Kirya <kkodolov40@gmail.com>
2025-09-14 13:10:45 +0300

    фикс регистрации

--- src/pages/login/RegisterPage/index.jsx
+++ src/pages/login/RegisterPage/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "@/contexts/auth/AuthContext";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { API_BASE } from "@/api/config";
 import { apiFetch } from "@/lib/apiClient";
 import Avatar from "@/components/small-components/Avatar";
