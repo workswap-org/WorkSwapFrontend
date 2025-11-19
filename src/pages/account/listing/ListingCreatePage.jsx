f7580f18817e8935577fc7202462cda5fa328e0b
Kirya <kkodolov40@gmail.com>
2025-11-19 09:55:29 +0300

    Фикс страницы создания объявления и фикс модалки переводов

--- src/pages/account/listing/ListingCreatePage.jsx
+++ src/pages/account/listing/ListingCreatePage.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { 
     useNotification,
     createListing,
--- src/pages/account/listing/ListingManagePage/menus/ListingSettingsMenu/translations/TranslationModal.jsx
+++ src/pages/account/listing/ListingManagePage/menus/ListingSettingsMenu/translations/TranslationModal.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Modal } from '@core/components';
 
 const TranslationModal = ({
     currentLang,
@@ -39,35 +40,36 @@ const TranslationModal = ({
     if (!currentLang) return null;
 
     return (
-        <div className="modal-overlay">
-            <div className="modal">
-                <div className="modal-content">
-                    <label>
-                        {t(`labels.title`, { ns: 'common' })} ({t(`languages.${currentLang}`, { ns: 'common' })}):
-                        <input
-                            type="text"
-                            value={title}
-                            maxLength={250}
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                    </label>
+        <Modal
+            isOpen={currentLang}
+            onClose={() => setCurrentLang(null)}
+            id="translationModal"
+        >
+            <label className='form-group'>
+                {t(`labels.title`, { ns: 'common' })} ({t(`languages.${currentLang}`, { ns: 'common' })}):
+                <input
+                    type="text"
+                    value={title}
+                    maxLength={250}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            </label>
 
-                    <label>
-                        {t(`labels.description`, { ns: 'common' })} ({t(`languages.${currentLang}`, { ns: 'common' })}):
-                        <textarea
-                            rows="4"
-                            value={description}
-                            maxLength={1900}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </label>
+            <label className='form-group' id="listingDescription">
+                {t(`labels.description`, { ns: 'common' })} ({t(`languages.${currentLang}`, { ns: 'common' })}):
+                <textarea
+                    id="listingDescriptionTxt"
+                    rows="4"
+                    value={description}
+                    maxLength={1900}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </label>
 
-                    <button type="button" className="btn btn-primary" onClick={handleAddLanguage}>
-                        {t(`listing.saveTranslation`, { ns: 'buttons' })}
-                    </button>
-                </div>
-            </div>
-        </div>
+            <button type="button" className="btn btn-primary" onClick={handleAddLanguage}>
+                {t(`listing.saveTranslation`, { ns: 'buttons' })}
+            </button>
+        </Modal>
     );
 };
 
