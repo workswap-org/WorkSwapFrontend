2788d139cdb85310f97ef43462e34502d8e53778
Kirya <kkodolov40@gmail.com>
2026-03-10 07:05:03 +0000

    Настройка лоадера на странице моих объявлений

--- packages/production/src/app/[locale]/account/my-listings/page.tsx
+++ packages/production/src/app/[locale]/account/my-listings/page.tsx
@@ -8,6 +8,7 @@ import PrivateListingCard from "@/components/ui/cards/listing-cards/PrivateListi
 import ListingDraftItem from "@/components/ui/cards/listing-cards/ListingDraftItem";
 import Tooltip from "@core/components/common/Tooltip"
 import { useI18n } from "@core/lib/contexts/I18nContext";
+import Loader from "@core/components/common/Loader"
 
 const MyListingsPage = () => {
 
@@ -25,7 +26,7 @@ const MyListingsPage = () => {
             setActiveListings(data.filter(listing => !listing.temporary));
             setDrafts(data.filter(listing => listing.temporary));
         })
-    }, [])
+    }, []);
 
     return (
         <>
@@ -39,9 +40,9 @@ const MyListingsPage = () => {
                 </button>
             </div>
 
-            {!loading && (
+            <Loader loadingActive={loading}>
                 <>
-                    {listings?.length == 0 && !loading ? (
+                    {listings?.length == 0 ? (
                         <div className="listings-grid">
                             <article onClick={() => redirect("/account/listing/create")} className="listing-card hover-animation-card">
                                 <div className="center">
@@ -84,8 +85,7 @@ const MyListingsPage = () => {
                         </>
                     )}
                 </>
-            )}
-            
+            </Loader>
         </>
     );
 };
